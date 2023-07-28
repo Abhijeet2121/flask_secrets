656e1d5be695245a7be207ae8cfa1c66af0aa4bc
Abhijeet2121 <kabhi30@gmail.com>
2023-07-29 00:55:15 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, redirect, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, email_validator
+from wtforms.validators import DataRequired, Length
 from flask_bootstrap import Bootstrap
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email(), email_validator()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField("Log In")
 
